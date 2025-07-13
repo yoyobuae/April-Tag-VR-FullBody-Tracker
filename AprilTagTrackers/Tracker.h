eb22731d5fca5b08e43d0b3d6506b445f0838b32
Yoyobuae <supersayoyin@gmail.com>
2025-07-12 20:15:16 -0500

    Make the out output window size smaller to reduce CPU use

--- AprilTagTrackers/Tracker.h
+++ AprilTagTrackers/Tracker.h
@@ -272,7 +272,7 @@ public:
     Quaternion<double> wrotation = Quaternion<double>(1, 0, 0, 0);
 
     double calibScale = 1;
-    int drawImgSize = 1385;
+    int drawImgSize = 675;
     bool rotate = false;
     int rotateFlag = -1;
 
