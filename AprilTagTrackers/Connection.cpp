0284dab7b23e2410f8303e1fe3317538b5cad017
Yoyobuae <supersayoyin@gmail.com>
2025-07-12 19:34:25 -0500

    Increase max saved previous positions used for interpolation

--- AprilTagTrackers/Connection.cpp
+++ AprilTagTrackers/Connection.cpp
@@ -281,7 +281,7 @@ void Connection::Connect()
     sstr += "settings 120 " + std::to_string(parameters->smoothingFactor);
     */
 
-    ret = Send("settings 120 " + std::to_string(parameters->smoothingFactor) + " " + std::to_string(parameters->additionalSmoothing));
+    ret = Send("settings 200 " + std::to_string(parameters->smoothingFactor) + " " + std::to_string(parameters->additionalSmoothing));
     //set that connection is established
     status = CONNECTED;
 
