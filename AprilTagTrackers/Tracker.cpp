68d4dda5ce80aca811b53d9a478a03ba5b6c1e5c
Yoyobuae <supersayoyin@gmail.com>
2026-01-20 02:12:39 -0500

    Tracker: Guard against zero sized ids array

--- AprilTagTrackers/Tracker.cpp
+++ AprilTagTrackers/Tracker.cpp
@@ -2904,7 +2904,7 @@ void Tracker::MainLoop()
             try
             {
                 trackerStatus[i].boardTvec /= calibScale;
-                if (cv::aruco::estimatePoseBoard(corners, ids, trackers[connection->connectedTrackers[i].TrackerId], parameters->camMat, parameters->distCoeffs, trackerStatus[i].boardRvec, trackerStatus[i].boardTvec, trackerStatus[i].boardFound && parameters->usePredictive) <= 1)
+                if (ids.size() == 0 || cv::aruco::estimatePoseBoard(corners, ids, trackers[connection->connectedTrackers[i].TrackerId], parameters->camMat, parameters->distCoeffs, trackerStatus[i].boardRvec, trackerStatus[i].boardTvec, trackerStatus[i].boardFound && parameters->usePredictive) <= 1)
                 {
                     for (int j = 0; j < 6; j++)
                     {
